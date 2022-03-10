3ef04fb829c6c0879caaacb811a886872578fda0
Augustas :-) <augustas_age@hotmail.com>
2022-03-10 23:49:35 +0100

    add certain elements to the about page
    adds the general strucuture of the code in the about.jsx file, also adds an about.css imported to app.css

--- src/AboutPage/About.jsx
+++ src/AboutPage/About.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { Container, Row, Col} from "react-bootstrap";
 
-export const About = () =>(
+const About = () =>{
+    return(
     <div>
-        <p>about page</p>
+        <Container>
+            <Row>
+                <Col>
+                <p>Hi!
+                    I am a soon-to-be Software Engineer graduate that is eager to make the world a better, 
+                    more efficient place. I was born in Lithuania but have lived most of my life in Sweden, 
+                    and I am currently based in Gothenburg. My educational background has taught me the value of 
+                    good code as well as understanding appropriate solutions based on stakeholder requirements. 
+                    However, good code is just as important as a pleasant UI/UX design. I aim to understand both these fields and create a flawless product and service experience.</p>
+                </Col>
+            </Row>
+            <Row>
+                <div/>
+                <p>Picture</p>
+                <div/>
+            </Row>
+            <Row>
+                <div/>
+            </Row>
+        </Container>
     </div>
-)
+    )
+}
+
+export default About;
--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import {Route, Routes, BrowserRouter as Router } from 'react-router-dom';
 import Home from './HomePage/Home';
-import {About} from './AboutPage/About';
+import About from './AboutPage/About';
 import {Experience} from './ExperiencePage/Experience';
 import NavBar from './Shared/NavBar';
 import Helmet from 'react-helmet';
